823ff5ddade274ba9052b3e043dfcc288ac29148
xfounder247 <xfounder247@gmail.com>
2026-05-18 15:26:15 +0530

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -7,7 +7,7 @@ import Signup from './pages/Signup'
 import Matches from './pages/Matches'
 import Listings from './pages/Listings'
 import Chat from './pages/Chat'
-import Profile from './pages/Profile'
+import Profile from './pages/Profile'import EditProfile from './pages/EditProfile'
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth()
@@ -31,7 +31,7 @@ const App = () => {
           <Route path="/matches" element={<PrivateRoute><Matches /></PrivateRoute>} />
           <Route path="/listings" element={<PrivateRoute><Listings /></PrivateRoute>} />
           <Route path="/chat" element={<PrivateRoute><Chat /></PrivateRoute>} />
-          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} /> <Route path="/edit-profile" element={<PrivateRoute><EditProfile /></PrivateRoute>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
